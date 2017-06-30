054c3ba6931ed1bd4493867d06e4d938180e4d68
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-30 17:44:10 +0200

    properly implement test for this

--- tests/css.php
+++ tests/css.php
@@ -2,30 +2,34 @@
 
 /** Test adding inline CSS */
 return function () : Generator {
-    /** Test a simple message */
+    /** Test a message with CSS */
     yield function () : Generator {
         $loader = new Twig_Loader_Array([
             'mail' => <<<EOT
 {% extends 'template' %}
-{% block subject %}Testing {{ product }}!{% endblock subject %}
+{% block subject %}Testing{% endblock subject %}
 {% block sender %}marijn@monomelodies.nl{% endblock sender %}
 {% block sender_name %}Marijn Ophorst{% endblock sender_name %}
 {% block html_content %}
-    <p>Hi there!</p>
+<a href></a>
+<div>
+    <a href></a>
+    <p>
+        <a href></a>
+    </p>
+</div>
 {% endblock html_content %}
 
 EOT
             ,
             'template' => <<<EOT
 {% block html %}
+<!doctype html>
 <html>
     <body>
-        <h1>Test template for {{ product }}</h1>
         {% block html_content %}{% endblock html_content %}
-        <small>hugs and kisses</small>
     </body>
 </html>
-
 {% endblock html %}
 
 EOT
@@ -34,70 +38,35 @@ EOT
             $loader,
             ['cache' => false, 'debug' => true]
         );
-        $msg = new Monomelodies\Emily\Message($twig);
+        $css = <<<EOT
+div {
+    border: 1px solid red;
+    }
+div p, div a {
+    color: blue;
+    }
+div p a {
+    font-weight: bold;
+    }
+EOT;
+        $msg = new Monomelodies\Emily\Message($twig, $css);
         $msg->loadTemplate('mail');
-        $msg->setVariables(['product' => 'Emily']);
-
-        /** Subject is set correctly */
-        yield function () use ($msg) {
-            assert('Testing Emily!' == $msg->getSubject());
-        };
-        /** Sender is set correctly */
-        yield function () use ($msg) {
-            assert('marijn@monomelodies.nl' == $msg->getSender());
-        };
-        $msg->setVariables(['product' => 'Swift_Mailer']);
-        /** Subject can be overridden */
-        yield function () use ($msg) {
-            assert('Testing Swift_Mailer!' == $msg->getSubject());
-        };
-        /** Body is set correctly */
-        yield function () use ($msg) {
-            assert(<<<EOT
-Test template for Swift_Mailer
-
-Hi there!
-
-hugs and kisses
-EOT
-                == $msg->getBody()
-            );
-        };
-    };
-
-    /** Test mixing plain and html */
-    yield function () : Generator {
-        $loader = new Twig_Loader_Array([
-            'mail' => <<<EOT
-{% extends 'template' %}
-{% block html_content %}
-<p>Hi there!</p>{% endblock html_content %}
-
-EOT
-            ,
-            'template' => <<<EOT
-{% block html %}
-{% block html_content %}{% endblock html_content %}
-{% endblock html %}
-
-{% block plain %}
-This will contain something different.{% endblock plain %}
-
-EOT
-        ]);
-        $twig = new Twig_Environment(
-            $loader,
-            ['cache' => false, 'debug' => true]
-        );
-        $msg = new Monomelodies\Emily\Message($twig);
-        $msg->loadTemplate('mail');
+        $expected = <<<EOT
+<!doctype html>
+<html><body>
+        <a href></a>
+<div style="border: 1px solid red;">
+    <a href style="color: blue;"></a>
+    <p style="color: blue;">
+        <a href style="color: blue; font-weight: bold;"></a>
+    </p>
+</div>
+    </body></html>
+EOT;
         /** The HTML part is set correctly */
-        yield function () use ($msg) {
-            assert('<p>Hi there!</p>' == $msg->getHtml());
-        };
-        /** The plain text part is set correctly */
-        yield function () use ($msg) {
-            assert('This will contain something different.' == $msg->getBody());
+        yield function () use ($msg, $expected) {
+            $html = $msg->getHtml();
+            assert($html == $expected);
         };
     };
 };
